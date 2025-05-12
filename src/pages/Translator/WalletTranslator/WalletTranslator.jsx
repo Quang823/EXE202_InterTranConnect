eecc2df0c96caa624168afd0f426044e98fa8b8d
Nam <namnpse173557@fpt.edu.vn>
2025-05-12 11:35:00 +0700

    Wallet translator

---
+++ src/pages/Translator/WalletTranslator/WalletTranslator.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Wallet from "../../../components/Translator/Wallet/WalletTrans";
+const WalletTrans = () => {
+  return (
+    <div className="wallet-container">
+      <div className="content-container container mx-auto" >
+        <Wallet />
+      </div>
+    </div>
+  )
+};
+
+export default WalletTrans;
