c46a4ce419fb272eeeb98fe52542ec59f3330c43
Đỗ Minh Quang <quangdmse173565@fpt.edu.vn>
2025-06-10 10:09:53 +0700

    Update route Admin

--- src/routes/AdminRoute/AdminRoute.jsx
+++ src/routes/AdminRoute/AdminRoute.jsx
@@ -1,7 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-
+import Admin from "../../pages/Admin/Admin";
 const AdminRoute = () => {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<Admin />} />
+    </Routes>
+  );
 };
 
 export default AdminRoute;
