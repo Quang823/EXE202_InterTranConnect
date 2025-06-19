4edb5fb2ced00fff6c5ff25fddeede3dd65e5255
Đỗ Minh Quang <quangdmse173565@fpt.edu.vn>
2025-06-19 20:47:08 +0700

    Update sửa lỗi certificate detail

--- src/pages/Translator/TranslatorCertificate/CertificateDetailsPage.jsx
+++ src/pages/Translator/TranslatorCertificate/CertificateDetailsPage.jsx
@@ -54,15 +54,42 @@ const CertificateDetailsPage = () => {
   if (error || !certificates || certificates.length === 0) {
     return (
       <div className="certificate-detail-page">
-        <p className="certificate-detail-error">
-          Error: {error || "No certificates found"}
-        </p>
-        <button
-          className="certificate-detail-btn-secondary"
-          onClick={() => navigate(`/profile-translator/${user.id}`)}
-        >
-          Back to Profile
-        </button>
+        <div className="certificate-detail-page__background">
+          <div className="certificate-detail-page__gradient-orb cert-add-page__gradient-orb--1"></div>
+          <div className="certificate-detail-page__gradient-orb cert-add-page__gradient-orb--2"></div>
+          <div className="certificate-detail-page__gradient-orb cert-add-page__gradient-orb--3"></div>
+        </div>
+        <div className="certificate-detail-page__container">
+          <div className="certificate-detail-page-header">
+            <div className="certificate-detail-page-button-container">
+              <button
+                className="certificate-detail-page-back-button"
+                onClick={() => navigate(`/translator/edit_profile`)}
+              >
+                <ArrowLeft className="certificate-detail-page-back-button__icon" />
+                <span>Back to Profile</span>
+              </button>
+              <button
+                className="certificate-detail-page-back-button"
+                onClick={() => navigate(`/translator/add_certificate`)}
+              >
+                <span>Back to add new Certificate</span>
+                <Eye className="certificate-detail-page-back-button__icon cert-back-button__icon--view" />
+              </button>
+            </div>
+            <div className="certificate-detail-page-title">
+              <h1 className="certificate-detail-page-title__main">
+                Your Certificates
+              </h1>
+              <p className="certificate-detail-page-title__subtitle">
+                Enhance your professional profile with certification details
+              </p>
+            </div>
+          </div>
+          <div className="certificate-detail-page-content">
+            <p className="certificate-detail-error">No certificate yet</p>
+          </div>
+        </div>
       </div>
     );
   }
